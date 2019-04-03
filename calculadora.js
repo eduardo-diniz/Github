cf34e6f55f35c0391548c4a415040f2305d6b1e7
Eduardo Gomes <Eduardogomes@hotmail.com>
2019-04-03 20:17:06 -0300

    Resolves #4: Criar função de divisao

--- calculadora.js
+++ calculadora.js
@@ -13,3 +13,14 @@ function multiplicacao (num1, num2){
 
     return num1 * num2
 }
+function divisao (num1, num2){
+    if (num2 === 0){
+
+        return new Error ('Não é possivel dividir por 0')
+
+    }
+        return num1 / num2
+    }
+
+  
+    
